aa2d7bf9171c943d0f03e2321c00a7d381ca0147
Crislander94 <crhighlander94@gmail.com>
2021-07-27 14:53:48 -0500

    updata login activity

--- app/src/main/java/com/example/registrar/LoginActivity.java
+++ app/src/main/java/com/example/registrar/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.registrar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -22,7 +24,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         TXT_USR =findViewById(R.id.editTextTextPersonName);
+        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        TXT_USR.setText(preferences.getString("usuario",""));
+
         TXT_PASS =findViewById(R.id.editTextTextPassword);
+        SharedPreferences preferences_1 = getSharedPreferences("datos1", Context.MODE_PRIVATE);
+        TXT_PASS.setText(preferences.getString("password",""));
         BTN_NuevoRegistro = findViewById(R.id.btn_nuevo_usuario);
         BTN_ING = findViewById(R.id.button);
         BTN_ING.setOnClickListener(v -> {
@@ -30,6 +37,15 @@ public class LoginActivity extends AppCompatActivity {
             String V_PASS = TXT_PASS.getText().toString();
             if (V_USR.equals("admin") && V_PASS.equals("admin")){
                 Toast.makeText(getApplicationContext(),"Datos correcto",Toast.LENGTH_SHORT).show();
+                SharedPreferences preferencias = getSharedPreferences("datos", Context.MODE_PRIVATE);
+                SharedPreferences.Editor Obj_editor = preferencias.edit();
+                Obj_editor.putString("usuario",TXT_USR.getText().toString());
+                Obj_editor.commit();
+
+                SharedPreferences preferencias1 = getSharedPreferences("datos1", Context.MODE_PRIVATE);
+                SharedPreferences.Editor Obj_editor1 = preferencias1.edit();
+                Obj_editor1.putString("password",TXT_PASS.getText().toString());
+                Obj_editor1.commit();
             }else {
                 Toast.makeText(getApplicationContext(),"Usuario o contraseña incorrecta",Toast.LENGTH_SHORT).show();
             }
